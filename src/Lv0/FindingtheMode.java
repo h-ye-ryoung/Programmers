aea863ea21b3d703ca3e7e2e4a1db4731403e72d
hyeryoung <pot46@naver.com>
2024-07-08 14:16:40 +0900

    [Lv0] 다음에 올 숫자

--- src/Lv0/FindingtheMode.java
+++ src/Lv0/FindingtheMode.java
@@ -7,7 +7,7 @@ import java.util.Map;
 // 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요.
 // 최빈값이 여러 개면 -1을 return 합니다.
 
-class Finding_the_Mode {
+class FindingtheMode {
     public int solution(int[] array) {
 
         Map<Integer, Integer> countMap = new HashMap<>();
